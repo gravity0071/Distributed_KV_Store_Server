26a869bd7ef62ef362c249e864f52bec7083ff9f
Cecilia <kxw@bupt.cn>
2024-12-06 10:31:13 -0500

    delete3

--- cmake-build-debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wukangxin/Desktop/Distributed_KV_Store_Server/cmake-build-debug/_deps/googletest-src/googlemock
-# Build directory: /Users/wukangxin/Desktop/Distributed_KV_Store_Server/cmake-build-debug/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
